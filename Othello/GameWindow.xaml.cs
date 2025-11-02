7b5b34e8fb14debd3a058b02284ae790e241d6b8
Zaid Yassir Sultan <suza24gp@student.ju.se>
2025-11-02 16:02:30 +0100

    Add project files.

---
+++ Othello/GameWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Othello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
